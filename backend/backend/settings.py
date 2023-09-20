d6ea4ecd6baf2bea335e0f3ed6c03107351e0fd5
Сергей Конников <s.konnikov@yandex.ru>
2023-09-21 10:56:39 +1100

    Add Actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,20 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+# SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
+# DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['84.201.141.114', '127.0.0.1', 'localhost', 'sprint-taski.servehttp.com']
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
