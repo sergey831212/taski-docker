d20efdccf889d143299ca8e88d7fcd122038e104
Сергей Конников <s.konnikov@yandex.ru>
2023-09-21 11:45:30 +1100

    Add Actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split()
 
 
 # Application definition
